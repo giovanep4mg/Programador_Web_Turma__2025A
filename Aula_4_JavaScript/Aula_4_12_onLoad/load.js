41b9681248b36b83636a87f59bf31269d8a74337
giovanep4mg <giovaneteodoro79@gmail.com>
2025-02-27 07:09:19 -0300

    novas-aulas-casa

---
+++ Aula_4_JavaScript/Aula_4_12_onLoad/load.js
@@ -0,0 +1,16 @@
+
+
+// Função carreguei que será executada no carregamento da página
+function carreguei(){
+
+    // 
+    document.getElementById("log").innerHTML = "Carreguei <br>";
+}
+
+//
+function redimensionei(){
+
+    //
+    document.getElementById("log").innerHTML = "Redimensionei <br>";
+
+}
